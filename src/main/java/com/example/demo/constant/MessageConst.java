eb4a75149cc711998f5c0039ca16b0bb8fe5cfc9
Abdelrahman Farag <>
2025-03-09 16:39:20 +0900

    入力文字数のバリデーションを追加 siginエラーメッセージを修正

--- src/main/java/com/example/demo/constant/MessageConst.java
+++ src/main/java/com/example/demo/constant/MessageConst.java
@@ -1,6 +1,9 @@
 package com.example.demo.constant;
 
 public class MessageConst {
+
+    public static final String FORM_ERROR = "common.formError";
+
     public static final String LOGIN_WRONG_INPUT = "login.wrongInput";
 
     public static final String SIGNUP_EXISTED_LOGIN_ID = "signup.existedLoginId";
